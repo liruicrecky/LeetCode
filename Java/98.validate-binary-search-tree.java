47292ba9b7a5e7fc26722cd61a472aa0b97ae97a
akka <liruicrecky@gmail.com>
2019-05-30 07:29:55 +0800

    5.30 Day-83

---
+++ Java/98.validate-binary-search-tree.java
@@ -0,0 +1,88 @@
+/*
+ * @lc app=leetcode id=98 lang=java
+ *
+ * [98] Validate Binary Search Tree
+ *
+ * https://leetcode.com/problems/validate-binary-search-tree/description/
+ *
+ * algorithms
+ * Medium (25.27%)
+ * Likes:    1912
+ * Dislikes: 290
+ * Total Accepted:    405K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[2,1,3]'
+ *
+ * Given a binary tree, determine if it is a valid binary search tree (BST).
+ * 
+ * Assume a BST is defined as follows:
+ * 
+ * 
+ * The left subtree of a node contains only nodes with keys less than the
+ * node's key.
+ * The right subtree of a node contains only nodes with keys greater than the
+ * node's key.
+ * Both the left and right subtrees must also be binary search trees.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * ⁠   2
+ * ⁠  / \
+ * ⁠ 1   3
+ * 
+ * Input: [2,1,3]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 1   4
+ * / \
+ * 3   6
+ * 
+ * Input: [5,1,4,null,null,3,6]
+ * Output: false
+ * Explanation: The root node's value is 5 but its right child's value is 4.
+ * 
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> s = new Stack<>();
+        TreeNode node = root;
+        //long prev = Long.MIN_VALUE;
+        double prev = - Double.MAX_VALUE;
+
+        while (!s.isEmpty() || node != null) {
+            if (node != null) {
+                s.push(node);
+                node = node.left;
+            } else {
+                node = s.pop();
+                if (prev >= node.val) {
+                    return false;
+                }
+                prev = node.val;
+                node = node.right;
+            }
+        }
+        return true;
+    }
+}
+
