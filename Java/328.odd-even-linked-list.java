93d350371f1a46107d5f768b15f3bfcd11f48c5e
recky <liruicrecky@gmail.com>
2019-06-05 06:40:10 +0800

    6.5 Day-89

---
+++ Java/328.odd-even-linked-list.java
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=328 lang=java
+ *
+ * [328] Odd Even Linked List
+ *
+ * https://leetcode.com/problems/odd-even-linked-list/description/
+ *
+ * algorithms
+ * Medium (49.16%)
+ * Likes:    741
+ * Dislikes: 220
+ * Total Accepted:    147.9K
+ * Total Submissions: 300.5K
+ * Testcase Example:  '[1,2,3,4,5]'
+ *
+ * Given a singly linked list, group all odd nodes together followed by the
+ * even nodes. Please note here we are talking about the node number and not
+ * the value in the nodes.
+ * 
+ * You should try to do it in place. The program should run in O(1) space
+ * complexity and O(nodes) time complexity.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: 1->2->3->4->5->NULL
+ * Output: 1->3->5->2->4->NULL
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: 2->1->3->5->6->4->7->NULL
+ * Output: 2->3->6->7->1->5->4->NULL
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * The relative order inside both the even and odd groups should remain as it
+ * was in the input.
+ * The first node is considered odd, the second node even and so on ...
+ * 
+ * 
+ */
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList1(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
+    public ListNode oddEvenList(ListNode head) {
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = head;
+
+        ListNode cur = fakeHead.next, n = cur == null ? null : cur.next;
+        while (n != null && n.next != null) {
+            ListNode t = cur.next, tt = n.next.next;
+            cur.next = n.next;
+            n.next.next = t;
+            n.next = tt;
+            cur = cur.next;
+            n = n.next;
+
+        }
+
+
+        return fakeHead.next;
+    }
+}
+
