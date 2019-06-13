d50547e4c34aed9822b140e6890c827194100b88
recky <liruicrecky@gmail.com>
2019-06-14 07:11:19 +0800

    6.14 Day-98

---
+++ Java/190.reverse-bits.java
@@ -0,0 +1,99 @@
+/*
+ * @lc app=leetcode id=190 lang=java
+ *
+ * [190] Reverse Bits
+ *
+ * https://leetcode.com/problems/reverse-bits/description/
+ *
+ * algorithms
+ * Easy (31.36%)
+ * Likes:    565
+ * Dislikes: 180
+ * Total Accepted:    183.6K
+ * Total Submissions: 582.7K
+ * Testcase Example:  '00000010100101000001111010011100'
+ *
+ * Reverse bits of a given 32 bits unsigned integer.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:  00000010100101000001111010011100
+ * Output: 00111001011110000010100101000000
+ * Explanation: The input binary string 00000010100101000001111010011100
+ * represents the unsigned integer 43261596, so return 964176192 which its
+ * binary representation is 00111001011110000010100101000000.
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:  11111111111111111111111111111101
+ * Output: 10111111111111111111111111111111
+ * Explanation: The input binary string 11111111111111111111111111111101
+ * represents the unsigned integer 4294967293, so return 3221225471 which its
+ * binary representation is 10101111110010110010011101101001.
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Note that in some languages such as Java, there is no unsigned integer type.
+ * In this case, both input and output will be given as signed integer type and
+ * should not affect your implementation, as the internal binary representation
+ * of the integer is the same whether it is signed or unsigned.
+ * In Java, the compiler represents the signed integers using 2's complement
+ * notation. Therefore, in Example 2 above the input represents the signed
+ * integer -3 and the output represents the signed integer -1073741825.
+ * 
+ * 
+ * 
+ * 
+ * Follow up:
+ * 
+ * If this function is called many times, how would you optimize it?
+ * 
+ */
+public class Solution {
+    public int reverseBits(int n) {
+        int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            ret <<= 1;
+            ret |= n & 1;
+            n >>>= 1;
+        }
+        return ret;
+    }
+
+    public int reverseBits2(int n) {
+        int ret = n;
+        ret = ret >>> 16 | ret << 16;
+        ret = (ret & 0xff00ff00) >>> 8 | (ret & 0x00ff00ff) << 8;
+        ret = (ret & 0xf0f0f0f0) >>> 4 | (ret & 0x0f0f0f0f) << 4;
+        ret = (ret & 0xcccccccc) >>> 2 | (ret & 0x33333333) << 2;
+        ret = (ret & 0xaaaaaaaa) >>> 1 | (ret & 0x55555555) << 1;
+        return ret;
+    }
+    // you need treat n as an unsigned value
+    public int reverseBits1(int n) {
+        byte[] arr = new byte[32];
+        for (int i = 0; i < 32; i++) {
+            if ((n & 1) == 0) {
+                arr[i] = 0;
+            } else {
+                arr[i] = 1;
+            }
+            n >>= 1;
+        }
+
+        int sum = 0;
+        for (int i = 0; i < 32; ++i) {
+            sum |= (arr[i] << (31 - i));
+        }
+        return sum;
+    }
+}
+
