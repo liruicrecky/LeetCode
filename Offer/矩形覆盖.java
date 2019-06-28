4e2fc7cf526988cc7964cf40cc6b117971b7bfa7
recky <liruicrecky@gmail.com>
2019-06-28 21:31:34 +0800

    6.28  Day-112

---
+++ Offer/矩形覆盖.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int RectCover(int target) {
+        int[] memo = new int[target + 1];
+        return help(memo, target);
+    }
+    
+    private int help(int[] memo, int target) {
+        if (memo[target] > 0) {
+            return memo[target];
+        }
+        if (target == 0 || target == 1 || target == 2) {
+            return target;
+        }
+        
+        return memo[target] = help(memo, target - 1) + help(memo, target - 2);
+    }
+}
