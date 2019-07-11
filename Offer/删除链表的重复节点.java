0ebd73d9dd254bcbbf93384de020321919dea389
recky <liruicrecky@gmail.com>
2019-07-11 21:08:58 +0800

    7.11 Day-124

--- Offer/删除链表的重复节点.java
+++ Offer/删除链表的重复节点.java
@@ -8,8 +8,31 @@
     }
 }
 */
+// 1->2->3->3->4->4->5 
 public class Solution {
     public ListNode deleteDuplication(ListNode pHead) {
+        if (pHead == null || pHead.next == null) {
+            return pHead;
+        }
 
+        ListNode fakeHead = new ListNode(0);
+        fakeHead.next = pHead;
+
+        ListNode node = fakeHead, n = node.next;
+        while (n != null) {
+            if (n.next != null && n.val == n.next.val) {
+                while (n.next != null && n.val == n.next.val) {
+                    n = n.next;
+                }
+
+                node.next = n.next;
+                n = n.next;
+            } else {
+                node = n;
+                n = n.next;
+            }
+        }
+
+        return fakeHead.next;
     }
 }
